abf47ccdebc3ea7c1fde23e9ba6b1bba8b8c988a
Rudie de Smit <rddesmit@hotmail.com>
2015-05-03 20:12:17 +0200

    Tested the refactored things

--- app/api/DigitalOceanAPI.scala
+++ app/api/DigitalOceanAPI.scala
@@ -5,6 +5,7 @@ import models._
 import play.api.Play.current
 import play.api.libs.json.Json
 import play.api.libs.ws.WS
+import play.mvc.Http.Status._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -22,85 +23,85 @@ trait DigitalOceanAPI {
     WS.url("https://api.digitalocean.com/v2/account")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
       .get()
-      .map {
-      case result@play.api.mvc.Results.Ok => Left(Success("success" -> "Authenticated"))
-      case error => Right(Error("error" -> (error.json \ "message")
-        .asOpt[String].getOrElse(error.json.toString)))
-    }
+      .map(result => result.status match {
+      case OK => Left(Success("success" -> "Authenticated"))
+      case error => Right(Error("error" -> (result.json \ "message")
+        .asOpt[String].getOrElse(result.json.toString)))
+    })
 
   /** Retrieves a droplet. */
   def getDroplet(id: BigDecimal, apiKey: String): Result[CloudServer, (String, String)] =
     WS.url(baseUri + s"droplets/$id")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
       .get()
-      .map {
-      case result@play.api.mvc.Results.Ok => Left(Success((result.json \ "droplet").as[CloudServer]))
-      case error => Right(Error("error" -> (error.json \ "message")
-        .asOpt[String].getOrElse(error.json.toString)))
-    }
+      .map(result => result.status match {
+      case OK => Left(Success((result.json \ "droplet").as[CloudServer]))
+      case error => Right(Error("error" -> (result.json \ "message")
+        .asOpt[String].getOrElse(result.json.toString)))
+    })
 
   def getDroplets(apiKey: String): Result[List[CloudServer], (String, String)] =
     WS.url(baseUri + "droplets")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
       .get()
-      .map {
-      case result@play.api.mvc.Results.Ok => Left(Success((result.json \ "droplets").as[List[CloudServer]]))
-      case error => Right(Error("error" -> (error.json \ "message")
-        .asOpt[String].getOrElse(error.json.toString)))
-    }
+      .map(result => result.status match {
+      case OK => Left(Success((result.json \ "droplets").as[List[CloudServer]]))
+      case error => Right(Error("error" -> (result.json \ "message")
+        .asOpt[String].getOrElse(result.json.toString)))
+    })
 
   /** Creates a droplet. */
   def createDroplet(apiKey: String, droplet: Droplet): Result[(String, String), (String, String)] =
     WS.url(baseUri + "droplets")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
       .post(droplet toJson)
-      .map {
-      case play.api.mvc.Results.Accepted => Left(Success("success" -> "Droplet created"))
-      case error => Right(Error("error" -> (error.json \ "message")
-        .asOpt[String].getOrElse(error.json.toString)))
-    }
+      .map(result => result.status match {
+      case ACCEPTED => Left(Success("success" -> "Droplet created"))
+      case error => Right(Error("error" -> (result.json \ "message")
+        .asOpt[String].getOrElse(result.json.toString)))
+    })
 
   /** Adds a SSH key to the Digital Ocean account. */
   def addSSHKey(apiKey: String, sshKey: SSHKey): Result[BigDecimal, (String, String)] =
     WS.url(baseUri + "account/keys")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
       .post(sshKey toJson)
-      .map {
-      case result@play.api.mvc.Results.Created => Left(Success((result.json \ "ssh_key" \ "id").as[BigDecimal]))
-      case error => Right(Error("error" -> (error.json \ "message")
-        .asOpt[String].getOrElse(error.json.toString)))
-    }
+      .map(result => result.status match {
+      case CREATED => Left(Success((result.json \ "ssh_key" \ "id").as[BigDecimal]))
+      case error => Right(Error("error" -> (result.json \ "message")
+        .asOpt[String].getOrElse(result.json.toString)))
+    })
 
   /** Powers the droplet on. */
   def powerOnDroplet(id: String, apiKey: String): Result[(String, String), (String, String)] =
     WS.url(baseUri + s"droplets/$id/actions")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
       .post(Json.obj("type" -> "power_on"))
-      .map {
-      case play.api.mvc.Results.Created => Left(Success("success" -> "Server powered on"))
-      case error => Right(Error("error" -> (error.json \ "message")
-        .asOpt[String].getOrElse(error.json.toString)))
-    }
+      .map(result => result.status match {
+      case CREATED => Left(Success("success" -> "Server powered on"))
+      case error => Right(Error("error" -> (result.json \ "message")
+        .asOpt[String].getOrElse(result.json.toString)))
+    })
 
   /** Powers the droplet off. */
   def powerOffDroplet(id: String, apiKey: String): Result[(String, String), (String, String)] =
     WS.url(baseUri + s"droplets/$id/actions")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
       .post(Json.obj("type" -> "power_off"))
-      .map {
-      case play.api.mvc.Results.Created => Left(Success("success" -> "Server powered off"))
-      case error => Right(Error("error" -> (error.json \ "message")
-        .asOpt[String].getOrElse(error.json.toString)))
-    }
+      .map(result => result.status match {
+      case CREATED => Left(Success("success" -> "Server powered off"))
+      case error => Right(Error("error" -> (result.json \ "message")
+        .asOpt[String].getOrElse(result.json.toString)))
+    })
 
   /** Deletes the droplet. */
   def deleteDroplet(id: String, apiKey: String): Result[(String, String), (String, String)] =
     WS.url(baseUri + s"droplets/$id")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
       .delete()
-      .map {
-      case play.api.mvc.Results.NoContent => Left(Success("success" -> "Server deleted"))
-      case error => Right(Error("error" -> (error.json \ "message")
-        .asOpt[String].getOrElse(error.json.toString)))
-    }
+      .map(result => result.status match {
+      case NO_CONTENT => Left(Success("success" -> "Server deleted"))
+      case error => Right(Error("error" -> (result.json \ "message")
+        .asOpt[String].getOrElse(result.json.toString)))
+    })
 }
