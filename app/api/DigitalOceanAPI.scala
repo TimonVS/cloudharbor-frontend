584e126e63783464db7a8a675ef7141c19ce6eb8
Rudie de Smit <rddesmit@hotmail.com>
2015-05-03 16:02:16 +0200

    Finished refactoring, needs testing!

--- app/api/DigitalOceanAPI.scala
+++ app/api/DigitalOceanAPI.scala
@@ -6,6 +6,7 @@ import play.api.Play.current
 import play.api.libs.json.Json
 import play.api.libs.ws.WS
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Either, Left, Right}
 
@@ -17,26 +18,60 @@ trait DigitalOceanAPI {
 
   val baseUri = "https://api.digitalocean.com/v2/"
 
-  def addDroplet(apiKey: String, droplet: Droplet) =
+  def authenticate(apiKey: String): Result[(String, String), (String, String)] =
+    WS.url("https://api.digitalocean.com/v2/account")
+      .withHeaders("Authorization" -> s"Bearer $apiKey")
+      .get()
+      .map {
+      case result@play.api.mvc.Results.Ok => Left(Success("success" -> "Authenticated"))
+      case error => Right(Error("error" -> (error.json \ "message")
+        .asOpt[String].getOrElse(error.json.toString)))
+    }
+
+  /** Retrieves a droplet. */
+  def getDroplet(id: BigDecimal, apiKey: String): Result[CloudServer, (String, String)] =
+    WS.url(baseUri + s"droplets/$id")
+      .withHeaders("Authorization" -> s"Bearer $apiKey")
+      .get()
+      .map {
+      case result@play.api.mvc.Results.Ok => Left(Success((result.json \ "droplet").as[CloudServer]))
+      case error => Right(Error("error" -> (error.json \ "message")
+        .asOpt[String].getOrElse(error.json.toString)))
+    }
+
+  def getDroplets(apiKey: String): Result[List[CloudServer], (String, String)] =
+    WS.url(baseUri + "droplets")
+      .withHeaders("Authorization" -> s"Bearer $apiKey")
+      .get()
+      .map {
+      case result@play.api.mvc.Results.Ok => Left(Success((result.json \ "droplets").as[List[CloudServer]]))
+      case error => Right(Error("error" -> (error.json \ "message")
+        .asOpt[String].getOrElse(error.json.toString)))
+    }
+
+  /** Creates a droplet. */
+  def createDroplet(apiKey: String, droplet: Droplet): Result[(String, String), (String, String)] =
     WS.url(baseUri + "droplets")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
       .post(droplet toJson)
-      .map(result => result match {
-      case play.api.mvc.Results.Accepted => "success" -> "Droplet created"
-      case _ => (result.json \ "message")
-        .asOpt[String].getOrElse(result.json.toString)
-    })
+      .map {
+      case play.api.mvc.Results.Accepted => Left(Success("success" -> "Droplet created"))
+      case error => Right(Error("error" -> (error.json \ "message")
+        .asOpt[String].getOrElse(error.json.toString)))
+    }
 
-  def addSSHKey(apiKey: String, sshKey: SSHKey): Future[Either[BigDecimal, String]] =
+  /** Adds a SSH key to the Digital Ocean account. */
+  def addSSHKey(apiKey: String, sshKey: SSHKey): Result[BigDecimal, (String, String)] =
     WS.url(baseUri + "account/keys")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
       .post(sshKey toJson)
       .map {
-      case result@play.api.mvc.Results.Created => Left((result.json \ "ssh_key" \ "id").as[BigDecimal])
-      case error => Right((error.json \ "message")
-        .asOpt[String].getOrElse(error.json.toString))
+      case result@play.api.mvc.Results.Created => Left(Success((result.json \ "ssh_key" \ "id").as[BigDecimal]))
+      case error => Right(Error("error" -> (error.json \ "message")
+        .asOpt[String].getOrElse(error.json.toString)))
     }
 
+  /** Powers the droplet on. */
   def powerOnDroplet(id: String, apiKey: String): Result[(String, String), (String, String)] =
     WS.url(baseUri + s"droplets/$id/actions")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
@@ -47,6 +82,7 @@ trait DigitalOceanAPI {
         .asOpt[String].getOrElse(error.json.toString)))
     }
 
+  /** Powers the droplet off. */
   def powerOffDroplet(id: String, apiKey: String): Result[(String, String), (String, String)] =
     WS.url(baseUri + s"droplets/$id/actions")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
@@ -57,6 +93,7 @@ trait DigitalOceanAPI {
         .asOpt[String].getOrElse(error.json.toString)))
     }
 
+  /** Deletes the droplet. */
   def deleteDroplet(id: String, apiKey: String): Result[(String, String), (String, String)] =
     WS.url(baseUri + s"droplets/$id")
       .withHeaders("Authorization" -> s"Bearer $apiKey")
