c8046b727f0d986726d5cb9d772cfccdf48b31c1
Timon van Spronsen <timonvanspronsen@outlook.com>
2015-05-25 10:03:35 +0200

    Add caching tot Docker Hub api

--- public/dev/assets/components/directives/imageSearch/imageSearch.factory.js
+++ public/dev/assets/components/directives/imageSearch/imageSearch.factory.js
@@ -4,9 +4,11 @@ angular
   .module('util')
   .factory('dockerHubApi', dockerHubApi);
 
-function dockerHubApi ($http) {
+function dockerHubApi ($http, $cacheFactory) {
   var apiUrl = 'https://cloudharbor-reverse-proxy.herokuapp.com/https://registry.hub.docker.com/v1/search?q=',
     pages = 25
+
+  var $httpDefaultCache = $cacheFactory.get('$http');
 
   function Image (params) {
     angular.extend(this, params)
@@ -17,7 +19,7 @@ function dockerHubApi ($http) {
     if (!params.limit) params.limit = 25
     var pageNum = params.pageNum || (params.offset ? Math.ceil(params.offset/params.limit) : 1)
 
-    return $http.get(apiUrl + query + '&page=' + pageNum + '&n=' + params.limit)
+    return $http.get(apiUrl + query + '&page=' + pageNum + '&n=' + params.limit, { cache: true })
       .then(function (response) {
         if (response.status !== 200) return response
 
