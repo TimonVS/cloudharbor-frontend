1e05266e8a7fff4ebeb652094349c527f663f9c5
Timon van Spronsen <timonvanspronsen@outlook.com>
2015-05-30 12:58:20 +0200

    Set-up mock api

---
+++ public/dev/assets/app/serverManagement/serverManagement.ctrl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function serverManagementCtrl ($http) {
+  var vm = this
+}
+
+angular
+  .module('app.serverManagement')
+  .controller('serverManagementCtrl', serverManagementCtrl);
