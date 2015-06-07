72dc91ca3874441cf1ba87742ac6fb02356f45b9
Timon van Spronsen <timonvanspronsen@outlook.com>
2015-06-07 21:06:08 +0200

    Add start, stop and delete actions for servers

---
+++ public/dev/assets/components/directives/progressButton/progressButton.dir.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function progressButton () {
+  return {
+    restrict: 'AE',
+    templateUrl: 'components/directives/progressButton/progressButton.tpl.html',
+    transclude: true,
+    replace: true,
+    scope: {
+      action: '&',
+      size: '@',
+      type: '@',
+      icon: '@'
+    },
+    link: function ($scope, $element, $attr) {
+      if (!$scope.size) $scope.size = 'md'
+      if (!$scope.type) $scope.type = 'primary'
+
+      $element.bind('click', function () {
+        $attr.$set('disabled', true)
+        $scope.action().finally(function () {
+          $attr.$set('disabled', false)
+        })
+      })
+    }
+  }
+}
+
+angular
+  .module('app.util')
+  .directive('chProgressButton', progressButton)
