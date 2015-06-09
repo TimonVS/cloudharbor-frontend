0b890fc5fb424b43f2a2fdb058e9e7c60e29850d
Rudie de Smit <rddesmit@hotmail.com>
2015-06-09 11:58:18 +0200

    Added deploy call

---
+++ app/controllers/docker/DeployManagement.scala
@@ -0,0 +1,26 @@
+package controllers.docker
+
+import java.net.ConnectException
+
+import controllers.docker.ImagesManagement._
+import play.api.Play.current
+import play.api.libs.ws.WS
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+ * Created by Rudie on 8-6-2015.
+ */
+object DeployManagement extends DockerManagement {
+
+  def deploy(serverUrl: String, imageName: String) = withAuthAsync { implicit user => implicit request =>
+    WS.url(s"http://$managementUrl/deploy/$imageName")
+      .withHeaders(dockerInfo(serverUrl))
+      .post(request.body.asJson.get)
+      .map(forwardResponse)
+      .recover {
+      case _: ConnectException => InternalServerError(unavailableJsonMessage(IMAGES_MANAGEMENT))
+      case _ => InternalServerError(unexpectedError)
+    }
+  }
+}
