bd0ecf718ed4987e46511f8bf8db7b825bded636
Timon van Spronsen <timonvanspronsen@outlook.com>
2015-06-13 21:57:28 +0200

    Remove unnecessary code

--- public/dev/assets/app/serverManagement/cloudService.res.js
+++ public/dev/assets/app/serverManagement/cloudService.res.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function CloudServiceFactory ($resource, lodash) {
+function CloudServiceFactory ($resource) {
 
   var CloudService = $resource('/servermanagement/:action', {}, {
     serverOptions: {
@@ -31,48 +31,6 @@ function CloudServiceFactory ($resource, lodash) {
       }
     }
   })
-
-  function sortBySize() {
-    var sizes = ['2gb', '4gb', '512mb', '8gb', '1gb']
-    var initial = sizes.pop()
-    var sorted = [initial]
-
-    var SizeEnum = Object.freeze({
-      'KB': {
-        value: 0,
-        name: 'KB'
-      },
-      'MB': {
-        value: 1,
-        name: 'MB'
-      },
-      'GB': {
-        value: 2,
-        name: 'GB'
-      },
-      'TB': {
-        value: 3,
-        name: 'TB'
-      }
-    })
-
-    String.prototype.getSizeExtension = function () {
-      return this.substring(this.length - 2).toUpperCase()
-    }
-
-    sizes.forEach(function (item) {
-      if (SizeEnum[initial.getSizeExtension()].value === SizeEnum[item.getSizeExtension()].value) {
-        if (item.slice(0, -2) >= initial.slice(0, -2)) sorted.push(item)
-        else sorted.unshift(item)
-      }
-      else if (SizeEnum[initial.getSizeExtension()].value >= SizeEnum[item.getSizeExtension()].value) {
-        sorted.unshift(item)
-      }
-      else if (SizeEnum[initial.getSizeExtension()].value < SizeEnum[item.getSizeExtension()].value) {
-        sorted.push(item)
-      }
-    })
-  }
 
   return CloudService
 
