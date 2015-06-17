211fa2d14fb9f4cb7563bff8c534c0f80ab056bb
Thomas Meijers <thomasmeijers@live.nl>
2015-06-17 17:58:37 +0200

    Fix password hash

--- app/models/UserProfile.scala
+++ app/models/UserProfile.scala
@@ -1,6 +1,7 @@
 package models
 
 import play.api.libs.json.Json
+import org.mindrot.jbcrypt.BCrypt
 
 /**
  * Created by ThomasWorkBook on 17/06/15.
@@ -14,11 +15,11 @@ case class UserProfile(
   lastName: String) {
 
   def save(user: User) = password.map { p =>
-    user.copy(user.id, username, p, email, firstName, prefix, lastName).save()
+    user.copy(user.id, username, BCrypt.hashpw(p, BCrypt.gensalt(12)), email, firstName, prefix, lastName).save()
   } getOrElse user.copy(user.id, username, user.password, email, firstName, prefix, lastName).save()
 
   def create: Either[String, User] = password map { p =>
-    Right(User.create(username, p, email, firstName, prefix, lastName).save())
+    Right(User.create(username, BCrypt.hashpw(p, BCrypt.gensalt(12)), email, firstName, prefix, lastName).save())
   } getOrElse Left("Password needs to be specified!")
 
 }
