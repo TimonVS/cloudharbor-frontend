4ab8c952da10496208a441a03afdc4f9cd65686e
Rudie de Smit <rddesmit@hotmail.com>
2015-06-18 10:40:59 +0200

    Added status to notification body's

--- app/models/Notifications/ServerNotification.scala
+++ app/models/Notifications/ServerNotification.scala
@@ -1,16 +1,17 @@
 package models.Notifications
 
 import play.api.libs.functional.syntax._
-import play.api.libs.json.{JsPath, Writes}
+import play.api.libs.json._
 
 /**
  * Created by Rudie on 10-6-2015.
  */
-case class ServerNotification(serverId: String, body: String)
+case class ServerNotification(serverId: String, body: String, status: String)
 
 object ServerNotification {
   implicit val serverNotificationWrites: Writes[ServerNotification] = (
-    (JsPath \ "id").write[String] and
-      (JsPath \ "body").write[String]
+    (__ \ "id").write[String] and
+      (__ \ "body").write[String] and
+      (__ \ "status").write[String]
     )(unlift(ServerNotification.unapply))
 }
